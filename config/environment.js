41b491900eccd13be9792682339a519fe569602d
Elliot Yandzio <elliotyandzio@gmail.com>
2018-04-27 09:58:22 +0100

    added heroku db connection

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,4 @@
-const databaseURI = 'mongodb://localhost/project2';
-const port = 3000;
+const databaseURI = process.env.MONGODB_URI || 'mongodb://localhost/project2';
+const port = process.env.port || 3000;
+
 module.exports ={databaseURI, port};
